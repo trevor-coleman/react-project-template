08fc4f741d3b40d597edc0d61ffb5dde9621e25a
Trevor Coleman <trevor@thesketchedarrow.com>
2016-10-11 06:26:09 -0400

    fixed action

--- src/actions.js
+++ src/actions.js
@@ -59,7 +59,6 @@ export const receiveChores = (chores) => {
 }
 
 export const fetchChores = () => {
-  dispatch(requestChores())
   return function(dispatch) {
     console.log("fetchChores")
     ChoresRef.on('value', snapshot => {
